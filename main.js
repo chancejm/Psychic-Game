f20d45c47b7d5829860a6c66085501e563c2d9ac
chancemusselman <musselman.chance@icloud.com>
2018-05-18 21:55:55 -0700

    Removed ConsoleLog that gave away answers

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-// variables used 
 var wins = 0;
 var losses = 0;
 var chance = 9;
@@ -15,7 +13,6 @@ var reset = function () {
     document.getElementById('guesses').innerHTML = chance;
     document.getElementById('userguessed').innerHTML = '__ __ __ __ __ __ __ __ __';
     computerGuess = letters[Math.floor(Math.random() * letters.length)];
-    console.log(computerGuess);
 }
 
 reset();
